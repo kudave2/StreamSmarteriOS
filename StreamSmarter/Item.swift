c9250fe2720a30053c47908246c32699c171ea5e
kudave2 <kudave2@yahoo.com>
2026-04-27 11:08:53 -0500

    Initial Commit

---
+++ StreamSmarter/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  StreamSmarter
+//
+//  Created by David Bush on 4/27/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ StreamSmarterTests/StreamSmarterTests.swift
@@ -0,0 +1,19 @@
+//
+//  StreamSmarterTests.swift
+//  StreamSmarterTests
+//
+//  Created by David Bush on 4/27/26.
+//
+
+import Testing
+@testable import StreamSmarter
+
+struct StreamSmarterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
